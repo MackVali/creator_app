1d8de24021cba50d7ef2f21ebfabfb83a304786e
MackVali <mackvali19@gmail.com>
2025-08-22 01:39:15 -0500

    refactor: use app router only

--- src/pages/coming-soon.tsx
+++
@@ -1,8 +0,0 @@
-export default function ComingSoon() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-950 text-gray-200">
-      <h1 className="text-2xl">Coming Soon…</h1>
-    </div>
-  );
-}
-
--- src/pages/create.tsx
+++
@@ -1,8 +0,0 @@
-export default function CreatePage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-950 text-gray-200">
-      <h1 className="text-2xl">Create Page</h1>
-    </div>
-  );
-}
-
