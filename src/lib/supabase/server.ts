92b9e1e4e90e0b7152d1c89e2efda45691d028ee
MackVali <mackvali19@gmail.com>
2025-08-23 17:59:57 -0500

    chore: configure supabase path aliases

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,24 +1,24 @@
-import { cookies } from "next/headers";
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { cookies } from 'next/headers';
+import { createServerClient } from '@supabase/ssr';
 
-export function createClient() {
+export const createClient = () => {
   const cookieStore = cookies();
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          return cookieStore.get(name)?.value;
+          return (cookieStore as any).get(name)?.value;
         },
-        set(name: string, value: string, options: CookieOptions) {
-          cookieStore.set({ name, value, ...options });
+        set(name: string, value: string, options: any) {
+          (cookieStore as any).set({ name, value, ...options });
         },
-        remove(name: string, options: CookieOptions) {
-          cookieStore.set({ name, value: "", ...options, maxAge: 0 });
+        remove(name: string, options: any) {
+          (cookieStore as any).set({ name, value: '', ...options });
         },
       },
-    },
+    }
   );
-}
+};
