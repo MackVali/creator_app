b7f92bc5212b049d1e8689caa3b9014df5f564cd
MackVali <mackvali19@gmail.com>
2025-08-25 11:34:09 -0500

    Add friends placeholder page

---
+++ src/app/(app)/friends/page.tsx
@@ -0,0 +1,7 @@
+export default function FriendsPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-950 text-gray-200">
+      <h1 className="text-2xl">Friends - Coming Soon…</h1>
+    </div>
+  );
+}
