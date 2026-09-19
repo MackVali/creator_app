c1f0013331e8adbaa826795fa8c1dae69646bd8c
MackVali <mackvali19@gmail.com>
2026-09-18 19:57:26 -0500

    Add ILAV check-in v1: src/app/(app)/ilav/page.tsx

---
+++ src/app/(app)/ilav/page.tsx
@@ -0,0 +1,9 @@
+import OperatorAiSheet from "@/components/ai/OperatorAiSheet";
+
+export default function IlavPage() {
+  return (
+    <main className="mx-auto flex h-[100dvh] w-full max-w-3xl flex-col bg-black text-white">
+      <OperatorAiSheet />
+    </main>
+  );
+}
